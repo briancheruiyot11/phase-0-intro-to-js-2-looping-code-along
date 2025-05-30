e6dfeec9d3331a8fe5c9c1ea3f978db02213b562
Brian Cheruiyot <briancheru11@gmail.com>
2025-05-31 01:27:30 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+function writeCards(names, eventName) {
+  const messages = [];
+  for (let i = 0; i < names.length; i++) {
+    messages.push(`Thank you, ${names[i]}, for the wonderful ${eventName} gift!`);
+  }
+
+  return messages;
+}
+console.log(writeCards(["Charlie", "Samip", "Ali"], "birthday"));
+
+function countDown(startNumber) {
+  let currentNumber = startNumber;
+
+  while (currentNumber >= 0) {
+    console.log(currentNumber);
+    currentNumber--;
+  }
+}
+
+countDown(10);
